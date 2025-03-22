dc085afed62dc7bcf9fa4039176aea92456195b1
sbaeumlisberger <s.baeumlisberger@live.de>
2025-03-22 13:44:46 +0100

    make RecycleBinEntry constructor internal

--- WindowsRecylceBin/RecycleBinEntry.cs
+++ WindowsRecylceBin/RecycleBinEntry.cs
@@ -3,12 +3,33 @@
 namespace WindowsRecylceBin;
 
 /// <summary>Represents an entry of the Windows recycle bin.</summary>
-/// <param name="OriginalFilePath">The original file path.</param>
-/// <param name="DeletedAt">The timestap when the file was deleted.</param>
-/// <param name="MetadataFilePath">The path of the metadata file in the recycle bin.</param>
-/// <param name="BackupFilePath">The path of the backup file in the recycle bin.</param>
-public record RecycleBinEntry(
-    string OriginalFilePath, 
-    DateTime DeletedAt, 
-    string MetadataFilePath,
-    string BackupFilePath);
+public record RecycleBinEntry
+{
+    /// <summary>
+    /// The original file path.
+    /// </summary>
+    public string OriginalFilePath { get; }
+
+    /// <summary>
+    /// The timestap when the file was deleted.
+    /// </summary>
+    public DateTime DeletedAt { get; }
+
+    /// <summary>
+    /// The path of the metadata file in the recycle bin.
+    /// </summary>
+    public string MetadataFilePath { get; }
+
+    /// <summary>
+    /// The path of the backup file in the recycle bin.
+    /// </summary>
+    public string BackupFilePath { get; }
+
+    internal RecycleBinEntry(string originalFilePath, DateTime deletedAt, string metadataFilePath, string backupFilePath)
+    {
+        OriginalFilePath = originalFilePath;
+        DeletedAt = deletedAt;
+        MetadataFilePath = metadataFilePath;
+        BackupFilePath = backupFilePath;
+    }
+}
